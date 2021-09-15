69f4812293dd8e4f53e91eb00dff4b340881208d
Federico Martin Alconada Verzini <fedealconada@gmail.com>
2021-09-15 17:02:06 +0100

    add base for Claim tests

---
+++ src/tests/Claim.t.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.8.6;
+
+import "./test-helpers/claim/ClaimTest.sol";
+
+contract Claim is ClaimTest {}
---
+++ src/tests/test-helpers/claim/ClaimTest.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.6;
+
+import "ds-test/test.sol";
+
+contract ClaimTest is DSTest {
+
+    function setUp() public {
+    }
+
+}
