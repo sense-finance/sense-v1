3f9d3c41893c4ef1fc5cb510239facab30e33c98
Josh Levine <joshuapark95@gmail.com>
2021-10-12 21:31:28 -0500

    refactor: use node module packages rather than dapp pkgs
    for consitency with the deploy scripts

--- src/external/FixedMath.sol
+++ src/external/FixedMath.sol
@@ -4,18 +4,26 @@ pragma solidity ^0.8.6;
 /// @title Fixed point arithmetic library
 /// @author Taken from https://github.com/yieldprotocol/yield-utils-v2/blob/main/contracts/math/WDiv.sol & https://github.com/yieldprotocol/yield-utils-v2/blob/main/contracts/math/WMul.sol
 library FixedMath {
-
     /// Taken from https://github.com/usmfum/USM/blob/master/contracts/FixedMath.sol
     /// @dev Multiply an amount by a fixed point factor with 18 decimals, rounds down
-    function fmul(uint256 x, uint256 y, uint256 baseUnit) internal pure returns (uint256 z) {
+    function fmul(
+        uint256 x,
+        uint256 y,
+        uint256 baseUnit
+    ) internal pure returns (uint256 z) {
         z = x * y;
-    unchecked { z /= baseUnit; }
+        unchecked {
+            z /= baseUnit;
+        }
     }
 
     /// Taken from https://github.com/usmfum/USM/blob/master/contracts/FixedMath.sol
     /// @dev Divide an amount by a fixed point factor with 18 decimals, rounds down
-    function fdiv(uint256 x, uint256 y, uint256 baseUnit) internal pure returns (uint256 z) {
+    function fdiv(
+        uint256 x,
+        uint256 y,
+        uint256 baseUnit
+    ) internal pure returns (uint256 z) {
         z = (x * baseUnit) / y;
     }
-
 }
--- src/tests/CFactory.tm.sol
+++ src/tests/CFactory.tm.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.6;
 
-
 // Internal references
 import { CFeed } from "../feeds/compound/CFeed.sol";
 import { CFactory } from "../feeds/compound/CFactory.sol";
