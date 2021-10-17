d30d719a6fa95eaa693640f07ef0e05e3c0ff384
Josh Levine <joshuapark95@gmail.com>
2021-10-17 19:04:17 -0400

    fix(wrappers): uint typo

--- src/wrappers/BaseTWrapper.sol
+++ src/wrappers/BaseTWrapper.sol
@@ -13,12 +13,11 @@ import { BaseFeed as Feed } from "../feeds/BaseFeed.sol";
 /// @notice
 contract BaseTWrapper is Initializable {
     using FixedMath for uint256;
-    /// @notice Configuration
 
-    /// @notice Mutable program state
-    address public target;
-    address public reward;
-    address public divider;
+    /// @notice Program state
+    address public immutable target;
+    address public immutable reward;
+    address public immutable divider;
     uint256 totalClaims;
     mapping(address => uint256) claimBalance;
 
@@ -30,14 +29,14 @@ contract BaseTWrapper is Initializable {
         target = _target;
         divider = _divider;
         reward = _reward;
-        ERC20(target).approve(divider, type(uin256).max); // approve max int
+        ERC20(target).approve(divider, type(uint256).max); // approve max int
         emit Initialized();
     }
 
     /* ========== MUTATIVE FUNCTIONS ========== */
     uint256 public share; // accumulated reward token per collected target
     uint256 public totalCollected; // total target collected
-    uint256 public lastRewardBal; // last recorded balance of reward token
+    uint256 public lastRewardBal;  // last recorded balance of reward token
 
     mapping(address => uint256) public crops; // reward token per collected target per user
     mapping(address => uint256) public collected; // collected target per user
@@ -45,6 +44,8 @@ contract BaseTWrapper is Initializable {
     function crop() internal virtual returns (uint256) {
         return ERC20(reward).balanceOf(address(this)) - lastRewardBal;
     }
+
+
 
     /// @notice Distributes rewarded tokens to Claim holders proportionally based on Claim balance
     /// @param _feed Feed to associate with the Series
@@ -68,7 +69,7 @@ contract BaseTWrapper is Initializable {
         claimBalance[_usr] = ERC20(claim).balanceOf(_usr);
     }
 
-    /// @notice Some protocols do not airdrop the reward token but the user needs to amake a call to claim them.
+    /// @notice Some protocols do not airdrop the reward token but the user needs to make a call to claim them.
     /// If so, must be overriden by child contracts
     function _claimReward() internal virtual {
         return;
