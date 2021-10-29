26af5721ba8c816f314b668e32da3be5a26c16ef
Federico Martin Alconada Verzini <fedealconada@gmail.com>
2021-10-29 13:08:17 +0100

    fix: remove unnecessary import

--- src/tests/test-helpers/mocks/MockPoolManager.sol
+++ src/tests/test-helpers/mocks/MockPoolManager.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.6;
 
-import { PoolManager } from "../../../fuse/PoolManager.sol";
-
-
 contract MockPoolManager {
     mapping(address => bool) public tInits; // Target Inits: target -> target added to pool
     mapping(address => mapping(uint256 => bool)) public sInits;
