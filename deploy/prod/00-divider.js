66ee0090dea48fdef9e6ebaeab7730dc7f69b4df
Josh Levine <joshuapark95@gmail.com>
2021-10-30 20:36:43 -0500

    fix(deploy:prod): sync prod scripts up with sim changes

--- deploy/prod/00-divider.js
+++ deploy/prod/00-divider.js
@@ -1,11 +1,3 @@
-const STAKE_TOKEN = new Map();
-// DAI Mainnet
-STAKE_TOKEN.set("1", "0x6b175474e89094c44da98b954eedeac495271d0f");
-// DAI Local Mainnet fork
-STAKE_TOKEN.set("111", "0x6b175474e89094c44da98b954eedeac495271d0f");
-// TODO: Arbitrum
-// UNISWAP_ROUTER.set("42161", "0x")
-
 const DIVIDER_CUP = new Map();
 // FIXME: real cup address (destination for unclaimed issuance fees)
 DIVIDER_CUP.set("1", "0x0000000000000000000000000000000000000000");
@@ -19,10 +11,7 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts, getCha
   const { deployer, dev } = await getNamedAccounts();
   const chainId = await getChainId();
 
-  if (!STAKE_TOKEN.has(chainId)) throw Error("No stake token found");
   if (!DIVIDER_CUP.has(chainId)) throw Error("No cup found");
-
-  const stake = STAKE_TOKEN.get(chainId);
   const cup = DIVIDER_CUP.get(chainId);
 
   const { address: assetDeployerAddress } = await deploy("AssetDeployer", {
@@ -34,7 +23,7 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts, getCha
   console.log("Deploy the divider");
   await deploy("Divider", {
     from: deployer,
-    args: [stake, cup, assetDeployerAddress],
+    args: [cup, assetDeployerAddress],
     log: true,
   });
 
