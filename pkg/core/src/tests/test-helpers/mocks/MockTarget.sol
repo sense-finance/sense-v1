37184fbce1df40c42e99b4ec52fa0760d7d4825e
Josh Levine <joshuapark95@gmail.com>
2022-01-05 23:57:28 -0500

    feat: add MultiMint helper, use authd mocks

--- pkg/core/src/tests/test-helpers/mocks/MockTarget.sol
+++ pkg/core/src/tests/test-helpers/mocks/MockTarget.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.11;
 
 // External references
-import { MockToken } from "./MockToken.sol";
+import { MockToken, AuthdMockToken } from "./MockToken.sol";
 
 contract MockTarget is MockToken {
     address public underlying;
@@ -13,6 +13,19 @@ contract MockTarget is MockToken {
         string memory _symbol,
         uint8 _decimal
     ) MockToken(_name, _symbol, _decimal) {
+        underlying = _underlying;
+    }
+}
+
+contract AuthdMockTarget is AuthdMockToken {
+    address public underlying;
+
+    constructor(
+        address _underlying,
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimal
+    ) AuthdMockToken(_name, _symbol, _decimal) {
         underlying = _underlying;
     }
 }
