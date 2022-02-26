a0762f20f9bd2130d1a69784b1d802a6de00db44
Josh Levine <joshuapark95@gmail.com>
2022-02-26 14:41:33 -0500

    refactor(core/external): swap out fixed math internals for efficiency and license considerations

--- pkg/core/src/external/FixedMath.sol
+++ pkg/core/src/external/FixedMath.sol
@@ -1,80 +1,81 @@
+// SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.11;
 
 /// @title Fixed point arithmetic library
-/// @author Taken from https://github.com/yieldprotocol/yield-utils-v2/blob/main/contracts/math/WDiv.sol & https://github.com/yieldprotocol/yield-utils-v2/blob/main/contracts/math/WMul.sol
+/// @author Taken from https://github.com/Rari-Capital/solmate/blob/main/src/utils/FixedPointMathLib.sol
 library FixedMath {
     uint256 internal constant WAD = 1e18;
     uint256 internal constant RAY = 1e27;
 
-    /// Taken from https://github.com/usmfum/USM/blob/master/contracts/WadMath.sol
-    /// @dev Multiply an amount by a fixed point factor, rounds down
-    function fmul(
+    function fmul(uint256 x, uint256 y, uint256 baseUnit) internal pure returns (uint256) {
+        return mulDivDown(x, y, baseUnit); // Equivalent to (x * y) / baseUnit rounded down.
+    }
+
+    function fmul(uint256 x, uint256 y) internal pure returns (uint256) {
+        return mulDivDown(x, y, WAD); // Equivalent to (x * y) / WAD rounded down.
+    }
+
+    function fmulUp(uint256 x, uint256 y, uint256 baseUnit) internal pure returns (uint256) {
+        return mulDivUp(x, y, baseUnit); // Equivalent to (x * y) / baseUnit rounded up.
+    }
+
+    function fmulUp(uint256 x, uint256 y) internal pure returns (uint256) {
+        return mulDivUp(x, y, WAD); // Equivalent to (x * y) / WAD rounded up.
+    }
+
+    function fdiv(uint256 x, uint256 y, uint256 baseUnit) internal pure returns (uint256) {
+        return mulDivDown(x, baseUnit, y); // Equivalent to (x * baseUnit) / y rounded down.
+    }
+
+    function fdiv(uint256 x, uint256 y) internal pure returns (uint256) {
+        return mulDivDown(x, WAD, y); // Equivalent to (x * WAD) / y rounded down.
+    }
+
+    function fdivUp(uint256 x, uint256 y, uint256 baseUnit) internal pure returns (uint256) {
+        return mulDivUp(x, baseUnit, y); // Equivalent to (x * baseUnit) / y rounded up.
+    }
+
+    function fdivUp(uint256 x, uint256 y) internal pure returns (uint256) {
+        return mulDivUp(x, WAD, y); // Equivalent to (x * WAD) / y rounded up.
+    }
+
+    function mulDivDown(
         uint256 x,
         uint256 y,
-        uint256 baseUnit
+        uint256 denominator
     ) internal pure returns (uint256 z) {
-        z = x * y;
-        unchecked {
-            z /= baseUnit;
+        assembly {
+            // Store x * y in z for now.
+            z := mul(x, y)
+
+            // Equivalent to require(denominator != 0 && (x == 0 || (x * y) / x == y))
+            if iszero(and(iszero(iszero(denominator)), or(iszero(x), eq(div(z, x), y)))) {
+                revert(0, 0)
+            }
+
+            // Divide z by the denominator.
+            z := div(z, denominator)
         }
     }
 
-    function fmul(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        z = x * y;
-        unchecked {
-            z /= WAD;
-        }
-    }
-
-    function fmulUp(
+    function mulDivUp(
         uint256 x,
         uint256 y,
-        uint256 baseUnit
+        uint256 denominator
     ) internal pure returns (uint256 z) {
-        z = x * y + (baseUnit - 1); // Rounds up.  So (again imagining 2 decimal places):
-        unchecked {
-            z /= baseUnit;
-        } // 383 (3.83) * 235 (2.35) -> 90005 (9.0005), + 99 (0.0099) -> 90104, / 100 -> 901 (9.01).
-    }
+        assembly {
+            // Store x * y in z for now.
+            z := mul(x, y)
 
-    function fmulUp(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        z = x * y + WAD - 1;
-        unchecked {
-            z /= WAD;
+            // Equivalent to require(denominator != 0 && (x == 0 || (x * y) / x == y))
+            if iszero(and(iszero(iszero(denominator)), or(iszero(x), eq(div(z, x), y)))) {
+                revert(0, 0)
+            }
+
+            // First, divide z - 1 by the denominator and add 1.
+            // We allow z - 1 to underflow if z is 0, because we multiply the
+            // end result by 0 if z is zero, ensuring we return 0 if z is zero.
+            z := mul(iszero(iszero(z)), add(div(sub(z, 1), denominator), 1))
         }
-    }
-
-    /// Taken from https://github.com/usmfum/USM/blob/master/contracts/WadMath.sol
-    /// @dev Divide an amount by a fixed point factor, rounds down
-    function fdiv(
-        uint256 x,
-        uint256 y,
-        uint256 baseUnit
-    ) internal pure returns (uint256 z) {
-        z = (x * baseUnit) / y;
-    }
-
-    function fdiv(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        z = (x * WAD) / y;
-    }
-
-    function fdivUp(
-        uint256 x,
-        uint256 y,
-        uint256 baseUnit
-    ) internal pure returns (uint256 z) {
-        z = x * baseUnit + y; // 101 (1.01) / 1000 (10) -> (101 * 100 + 1000 - 1) / 1000 -> 11 (0.11 = 0.101 rounded up).
-        unchecked {
-            z -= 1;
-        } // Can do unchecked subtraction since division in next line will catch y = 0 case anyway
-        z /= y;
-    }
-
-    function fdivUp(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        z = x * WAD + y;
-        unchecked {
-            z -= 1;
-        }
-        z /= y;
     }
 }
