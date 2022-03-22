7e593ab8fbbd457755f9970c7f3735254f845f71
Josh Levine <24902242+jparklev@users.noreply.github.com>
2022-03-22 01:26:06 -0400

    chore(core/tests): run 8 decimal tests for underlying as well (#214)
    * chore(core/tests): run 8 decimal tests for underlying as well
    
    * chore(ci): update 8 decimal CI step verbiage
    
    * fix(core/Divider): rmv uDecimals from reweightLScale calc (#215)
    
    * fix(core/Divider): rmv uDecimals from reweightLScale calc
    
    * chore(core/tests): rmv uDecimals from adapter meta expectations
    
    * fix(core/tests): rmv evil target
    
    * chore: appease linter
    
    * chore(core/tests): rmv evil target import, rename underlying decimal var
    
    * chore(core/tests): use different underlying and target decimal vals for decimals test suite
    
    * chore(core/Divider): add context to decimals comment
    
    * chore: appease linter
    
    * fix(deployments/simulated): set minbpt out to 0
    
    * chore(core/tests): set decimal vals for unusual decimal test suite from env

--- pkg/core/src/tests/Divider.t.sol
+++ pkg/core/src/tests/Divider.t.sol
@@ -11,7 +11,6 @@ import { Levels } from "@sense-finance/v1-utils/src/libs/Levels.sol";
 import { TestHelper } from "./test-helpers/TestHelper.sol";
 import { User } from "./test-helpers/User.sol";
 import { MockAdapter, MockBaseAdapter } from "./test-helpers/mocks/MockAdapter.sol";
-import { MockEvilTarget } from "./test-helpers/mocks/MockTarget.sol";
 import { BaseAdapter } from "../adapters/BaseAdapter.sol";
 import { CropAdapter } from "../adapters/CropAdapter.sol";
 import { Divider } from "../Divider.sol";
@@ -627,7 +626,7 @@ contract Dividers is TestHelper {
         divider.setGuarded(false);
         uint256 maturity = getValidMaturity(2021, 10);
         sponsorSampleSeries(address(alice), maturity);
-        (, , uint256 guard, , ) = divider.adapterMeta(address(adapter));
+        (, , uint256 guard, ) = divider.adapterMeta(address(adapter));
         alice.doIssue(address(adapter), maturity, guard + 1);
     }
 
@@ -1978,7 +1977,7 @@ contract Dividers is TestHelper {
     function testSetAdapterFirst() public {
         // check first adapter added on TestHelper.sol has ID 1
         assertEq(divider.adapterCounter(), 1);
-        (uint248 id, , , , ) = divider.adapterMeta(address(adapter));
+        (uint248 id, , , ) = divider.adapterMeta(address(adapter));
         assertEq(id, 1);
         assertEq(divider.adapterAddresses(1), address(adapter));
     }
@@ -2001,11 +2000,10 @@ contract Dividers is TestHelper {
         uint256 adapterCounter = divider.adapterCounter();
 
         divider.setAdapter(address(aAdapter), true);
-        (uint248 id, bool enabled, , uint256 uDecimals, ) = divider.adapterMeta(address(aAdapter));
+        (uint248 id, bool enabled, , ) = divider.adapterMeta(address(aAdapter));
         assertTrue(enabled);
         assertEq(id, adapterCounter + 1);
         assertEq(divider.adapterAddresses(adapterCounter + 1), address(aAdapter));
-        assertEq(uDecimals, underlying.decimals());
     }
 
     function testSetAdapterBackOnKeepsExistingId() public {
@@ -2027,7 +2025,7 @@ contract Dividers is TestHelper {
 
         // set adapter on
         divider.setAdapter(address(aAdapter), true);
-        (uint248 id, bool enabled, , , ) = divider.adapterMeta(address(aAdapter));
+        (uint248 id, bool enabled, , ) = divider.adapterMeta(address(aAdapter));
         assertTrue(enabled);
         assertEq(id, adapterCounter + 1);
         assertEq(divider.adapterAddresses(adapterCounter + 1), address(aAdapter));
@@ -2051,14 +2049,14 @@ contract Dividers is TestHelper {
             address(reward)
         );
         divider.setAdapter(address(bAdapter), true);
-        (id, enabled, , , ) = divider.adapterMeta(address(bAdapter));
+        (id, enabled, , ) = divider.adapterMeta(address(bAdapter));
         assertTrue(enabled);
         assertEq(id, adapterCounter + 2);
         assertEq(divider.adapterAddresses(adapterCounter + 2), address(bAdapter));
 
         // set adapter back on
         divider.setAdapter(address(aAdapter), true);
-        (id, enabled, , , ) = divider.adapterMeta(address(aAdapter));
+        (id, enabled, , ) = divider.adapterMeta(address(aAdapter));
         assertTrue(enabled);
         assertEq(id, adapterCounter + 1);
         assertEq(divider.adapterAddresses(adapterCounter + 1), address(aAdapter));
@@ -2121,39 +2119,9 @@ contract Dividers is TestHelper {
         );
         divider.setPermissionless(true);
         bob.doAddAdapter(address(aAdapter));
-        (uint248 id, bool enabled, , uint256 uDecimals, ) = divider.adapterMeta(address(adapter));
+        (uint248 id, bool enabled, , ) = divider.adapterMeta(address(adapter));
         assertEq(id, 1);
         assertEq(divider.adapterAddresses(1), address(adapter));
         assertTrue(enabled);
-        assertEq(uDecimals, underlying.decimals());
-    }
-
-    function testTargetAndUnderlyingCantMutateWithMaliciousAdapter() public {
-        MockEvilTarget underlying = new MockEvilTarget(address(0), "und", "UND");
-        MockEvilTarget target = new MockEvilTarget(address(underlying), "und", "UND");
-        MockAdapter aAdapter = new MockAdapter(
-            address(divider),
-            address(target),
-            ORACLE,
-            ISSUANCE_FEE,
-            address(stake),
-            STAKE_SIZE,
-            MIN_MATURITY,
-            MAX_MATURITY,
-            4,
-            0,
-            DEFAULT_LEVEL,
-            address(reward)
-        );
-        divider.setPermissionless(true);
-        bob.doAddAdapter(address(aAdapter));
-        (, , , uint256 uDecimals, ) = divider.adapterMeta(address(aAdapter));
-        uint256 prevUDecimals = uDecimals;
-        hevm.warp(block.timestamp + 10 days);
-        (, , , uDecimals, ) = divider.adapterMeta(address(aAdapter));
-        assertEq(prevUDecimals, uDecimals);
-        hevm.warp(block.timestamp + 10 days);
-        (, , , uDecimals, ) = divider.adapterMeta(address(aAdapter));
-        assertEq(prevUDecimals, uDecimals);
     }
 }
