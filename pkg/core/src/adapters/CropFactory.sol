752b73858bba1623a0a3f1fc8d77e6b4618fab88
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2022-04-12 17:56:46 +0100

    chore(CropFacctory): polish comments

--- pkg/core/src/adapters/CropFactory.sol
+++ pkg/core/src/adapters/CropFactory.sol
@@ -14,23 +14,11 @@ abstract contract CropFactory is Trust, BaseFactory {
         BaseFactory(_divider, _factoryParams)
     {}
 
-    // /// @notice Replace existing reward tokens array with a new one
-    // /// @dev Called by owner of the adapter factory, it will have an impact only on future deployed adapters
-    // /// @param _rewardTokens array of rewards tokens addresses
-    // function setRewardTokens(address[] memory _rewardTokens) public requiresTrust {
-    //     rewardTokens = _rewardTokens;
-    // }
-
-    // TODO: we could remove the `setRewardTokens` on top but we would need to send and empty array when deploying, e.g, CAdapter
     // TODO: do we want this to be a function on the Periphery rather than here?
-    /// @notice Replace existing reward tokens array with a new one
-    /// @dev Called by owner of the adapter factory, it will impact future deployed adapters and will also
-    /// update the adapters passed on `_adapters` array
+    /// @notice Replace existing reward tokens array with a new one and update adapters passed
     /// @param _rewardTokens array of rewards tokens addresses
     /// @param _adapters array of adapters to update the rewards tokens on
     function setRewardTokens(address[] memory _rewardTokens, address[] memory _adapters) public requiresTrust {
-        // rewardTokens = _rewardTokens;
-
         for (uint256 i = 0; i < _adapters.length; i++) {
             CropAdapter(_adapters[i]).setRewardTokens(_rewardTokens);
         }
