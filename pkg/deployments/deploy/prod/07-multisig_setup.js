f5162e1078bc46f4541c2967f118d4e4c2210109
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2022-05-31 16:57:18 +0100

    chore(Scripts): add check to prevent headless contracts

--- pkg/deployments/deploy/prod/07-multisig_setup.js
+++ pkg/deployments/deploy/prod/07-multisig_setup.js
@@ -2,64 +2,111 @@ const { SENSE_MULTISIG, OZ_RELAYER } = require("../../hardhat.addresses");
 const log = console.log;
 
 module.exports = async function () {
-  const { deployer } = await getNamedAccounts();
-  const signer = await ethers.getSigner(deployer);
+  const { deployer, dev } = await getNamedAccounts();
+  const deployerSigner = await ethers.getSigner(deployer);
   const chainId = await getChainId();
 
   if (!SENSE_MULTISIG.has(chainId)) throw Error("No sense multisig found");
-  const multisig = chainId === "5" ? OZ_RELAYER.get(chainId) : SENSE_MULTISIG.get(chainId);
+  const multisig = chainId === "5" ? OZ_RELAYER.get("5") : SENSE_MULTISIG.get(chainId);
 
-  const tokenHandler = await ethers.getContract("TokenHandler", signer);
-  const divider = await ethers.getContract("Divider", signer);
-  const poolManager = await ethers.getContract("PoolManager", signer);
-  const spaceFactory = await ethers.getContract("SpaceFactory", signer);
-  const periphery = await ethers.getContract("Periphery", signer);
-  const emergencyStop = await ethers.getContract("EmergencyStop", signer);
+  let tokenHandler = await ethers.getContract("TokenHandler", deployerSigner);
+  let divider = await ethers.getContract("Divider", deployerSigner);
+  let poolManager = await ethers.getContract("PoolManager", deployerSigner);
+  let spaceFactory = await ethers.getContract("SpaceFactory", deployerSigner);
+  let periphery = await ethers.getContract("Periphery", deployerSigner);
+  let emergencyStop = await ethers.getContract("EmergencyStop", deployerSigner);
 
   log("\n-------------------------------------------------------");
   log("\nAdd multisig as trusted address on contracts");
 
   log("Trust the multisig address on the token handler");
-  await (await tokenHandler.setIsTrusted(multisig, true)).wait();
+  if (!(await tokenHandler.isTrusted(multisig))) {
+    await (await tokenHandler.setIsTrusted(multisig, true)).wait();
+  }
 
   log("Trust the multisig address on the divider");
-  await (await divider.setIsTrusted(multisig, true)).wait();
+  if (!(await divider.isTrusted(multisig))) {
+    await (await divider.setIsTrusted(multisig, true)).wait();
+  }
 
   log("Trust the multisig address on the pool manager");
-  await (await poolManager.setIsTrusted(multisig, true)).wait();
+  if (!(await poolManager.isTrusted(multisig))) {
+    await (await poolManager.setIsTrusted(multisig, true)).wait();
+  }
 
   log("Trust the multisig address on the space factory");
-  await (await spaceFactory.setIsTrusted(multisig, true)).wait();
+  if (!(await spaceFactory.isTrusted(multisig))) {
+    await (await spaceFactory.setIsTrusted(multisig, true)).wait();
+  }
 
   log("Trust the multisig address on the periphery");
-  await (await periphery.setIsTrusted(multisig, true)).wait();
+  if (!(await periphery.isTrusted(multisig))) {
+    await (await periphery.setIsTrusted(multisig, true)).wait();
+  }
 
   log("Trust the multisig address on the emergency stop");
-  await (await emergencyStop.setIsTrusted(multisig, true)).wait();
+  if (!(await emergencyStop.isTrusted(multisig))) {
+    await (await emergencyStop.setIsTrusted(multisig, true)).wait();
+  }
+
+  // if multisig is same as deployer, we don't want to untrust it
+  if (multisig !== deployer) {
+
+    log("\n-------------------------------------------------------");
+    log("\nRemove deployer address from trusted on contracts");
+
+    log("Untrust deployer on the token handler");
+    await (await tokenHandler.setIsTrusted(deployer, false)).wait();
+
+    log("Untrust deployer on the divider");
+    await (await divider.setIsTrusted(deployer, false)).wait();
+
+    log("Untrust deployer on the pool manager");
+    await (await poolManager.setIsTrusted(deployer, false)).wait();
+
+    log("Untrust deployer on the space factory");
+    await (await spaceFactory.setIsTrusted(deployer, false)).wait();
+
+    log("Untrust deployer on the periphery");
+    await (await periphery.setIsTrusted(deployer, false)).wait();
+
+    log("Untrust deployer on the emergency stop");
+    await (await emergencyStop.setIsTrusted(deployer, false)).wait();
+
+    log("\n-------------------------------------------------------");
+    log("Sanity checks: deployer address cannot execute trusted functions anymore...");
+
+    const calls = [
+      tokenHandler.callStatic.setIsTrusted(dev, false),
+      divider.callStatic.setIsTrusted(dev, false),
+      poolManager.callStatic.setIsTrusted(dev, false),
+      spaceFactory.callStatic.setIsTrusted(dev, false),
+      periphery.callStatic.setIsTrusted(dev, false),
+      emergencyStop.callStatic.setIsTrusted(dev, false),
+    ];
+    const res = await Promise.allSettled(calls);
+    if (res.every(r => r.status === "rejected")) {
+      log("Sanity checks: OK!");
+    } else {
+      throw Error("Sanity checks: FAILED");
+    }
+  }
 
   log("\n-------------------------------------------------------");
-  log("\nRemove deployer address from trusted on contracts");
+  log("Sanity checks: multisig address can execute trusted functions...");
 
-  log("Untrust deployer on the token handler");
-  await (await tokenHandler.setIsTrusted(deployer, false)).wait();
+  const multisigSigner = await hre.ethers.getSigner(multisig);
+  tokenHandler = tokenHandler.connect(multisigSigner);
+  divider = divider.connect(multisigSigner);
+  poolManager = poolManager.connect(multisigSigner);
+  spaceFactory = spaceFactory.connect(multisigSigner);
+  periphery = periphery.connect(multisigSigner);
+  emergencyStop = emergencyStop.connect(multisigSigner);
 
-  log("Untrust deployer on the divider");
-  await (await divider.setIsTrusted(deployer, false)).wait();
-
-  log("Untrust deployer on the pool manager");
-  await (await poolManager.setIsTrusted(deployer, false)).wait();
-
-  log("Untrust deployer on the space factory");
-  await (await spaceFactory.setIsTrusted(deployer, false)).wait();
-
-  log("Untrust deployer on the periphery");
-  await (await periphery.setIsTrusted(deployer, false)).wait();
-
-  log("Untrust deployer on the emergency stop");
-  await (await emergencyStop.setIsTrusted(deployer, false)).wait();
-
-  log("\n-------------------------------------------------------");
-  log("Sanity checks: deployer address cannot execute trusted functions anymore...");
+  await hre.network.provider.request({
+    method: "hardhat_impersonateAccount",
+    params: [multisig],
+  });
 
   const calls = [
     tokenHandler.callStatic.setIsTrusted(dev, false),
@@ -70,11 +117,12 @@ module.exports = async function () {
     emergencyStop.callStatic.setIsTrusted(dev, false),
   ];
   const res = await Promise.allSettled(calls);
-  if (res.every(r => r.status === "rejected")) {
+  if (res.every(r => r.status === "fulfilled")) {
     log("Sanity checks: OK!");
   } else {
     throw Error("Sanity checks: FAILED");
   }
+
 };
 
 module.exports.tags = ["prod:multisig", "scenario:prod"];
--- pkg/deployments/deploy/simulated/07-multisig_setup.js
+++ pkg/deployments/deploy/simulated/07-multisig_setup.js
@@ -3,18 +3,18 @@ const log = console.log;
 
 module.exports = async function () {
   const { deployer, dev } = await getNamedAccounts();
-  const signer = await ethers.getSigner(deployer);
+  const deployerSigner = await ethers.getSigner(deployer);
   const chainId = await getChainId();
 
   if (!SENSE_MULTISIG.has(chainId)) throw Error("No sense multisig found");
-  const multisig = chainId === "5" ? OZ_RELAYER.get(chainId) : SENSE_MULTISIG.get(chainId);
+  const multisig = chainId === "5" ? OZ_RELAYER.get("5") : SENSE_MULTISIG.get(chainId);
 
-  const tokenHandler = await ethers.getContract("TokenHandler", signer);
-  const divider = await ethers.getContract("Divider", signer);
-  const poolManager = await ethers.getContract("PoolManager", signer);
-  const spaceFactory = await ethers.getContract("SpaceFactory", signer);
-  const periphery = await ethers.getContract("Periphery", signer);
-  const emergencyStop = await ethers.getContract("EmergencyStop", signer);
+  let tokenHandler = await ethers.getContract("TokenHandler", deployerSigner);
+  let divider = await ethers.getContract("Divider", deployerSigner);
+  let poolManager = await ethers.getContract("PoolManager", deployerSigner);
+  let spaceFactory = await ethers.getContract("SpaceFactory", deployerSigner);
+  let periphery = await ethers.getContract("Periphery", deployerSigner);
+  let emergencyStop = await ethers.getContract("EmergencyStop", deployerSigner);
 
   log("\n-------------------------------------------------------")
   log("\nAdd multisig as trusted address on contracts");
@@ -49,41 +49,74 @@ module.exports = async function () {
     await (await emergencyStop.setIsTrusted(multisig, true)).wait();
   }
 
-  log("\n-------------------------------------------------------")
-  log("\nRemove deployer address from trusted on contracts");
+  // if multisig is same as deployer, we don't want to untrust it
+  if (multisig !== deployer) {
+    log("\n-------------------------------------------------------")
+    log("\nRemove deployer address from trusted on contracts");
+    log("Untrust deployer on the token handler");
+    if (await tokenHandler.isTrusted(deployer)) {
+      await (await tokenHandler.setIsTrusted(deployer, false)).wait();  
+    }
 
-  log("Untrust deployer on the token handler");
-  if (await tokenHandler.isTrusted(deployer)) {
-    await (await tokenHandler.setIsTrusted(deployer, false)).wait();  
+    log("Untrust deployer on the divider");
+    if (await divider.isTrusted(deployer)) {
+      await (await divider.setIsTrusted(deployer, false)).wait();  
+    }
+
+    log("Untrust deployer on the pool manager");
+    if (await poolManager.isTrusted(deployer)) {
+      await (await poolManager.setIsTrusted(deployer, false)).wait();  
+    }
+
+    log("Untrust deployer on the space factory");
+    if (await spaceFactory.isTrusted(deployer)) {
+      await (await spaceFactory.setIsTrusted(deployer, false)).wait();  
+    }
+
+    log("Untrust deployer on the periphery");
+    if (await periphery.isTrusted(deployer)) {
+      await (await periphery.setIsTrusted(deployer, false)).wait();  
+    }
+
+    log("Untrust deployer on the emergency stop");
+    if (await emergencyStop.isTrusted(deployer)) {
+      await (await emergencyStop.setIsTrusted(deployer, false)).wait();  
+    }
+
+    log("\n-------------------------------------------------------")
+    log("\Sanity checks: checking deployer address cannot execute trusted functions anymore...");
+
+    const calls = [
+      tokenHandler.callStatic.setIsTrusted(dev, false),
+      divider.callStatic.setIsTrusted(dev, false),
+      poolManager.callStatic.setIsTrusted(dev, false),
+      spaceFactory.callStatic.setIsTrusted(dev, false),
+      periphery.callStatic.setIsTrusted(dev, false),
+      emergencyStop.callStatic.setIsTrusted(dev, false),
+    ];
+    const res = await Promise.allSettled(calls);
+    if (res.every(r => r.status === 'rejected')) {
+      log("\Sanity checks: OK!");
+    } else {
+      throw Error('Sanity checks: FAILED');
+    }
   }
 
-  log("Untrust deployer on the divider");
-  if (await divider.isTrusted(deployer)) {
-    await (await divider.setIsTrusted(deployer, false)).wait();  
-  }
+  log("\n-------------------------------------------------------");
+  log("Sanity checks: multisig address can execute trusted functions...");
 
-  log("Untrust deployer on the pool manager");
-  if (await poolManager.isTrusted(deployer)) {
-    await (await poolManager.setIsTrusted(deployer, false)).wait();  
-  }
+  const multisigSigner = await hre.ethers.getSigner(multisig);
+  tokenHandler = tokenHandler.connect(multisigSigner);
+  divider = divider.connect(multisigSigner);
+  poolManager = poolManager.connect(multisigSigner);
+  spaceFactory = spaceFactory.connect(multisigSigner);
+  periphery = periphery.connect(multisigSigner);
+  emergencyStop = emergencyStop.connect(multisigSigner);
 
-  log("Untrust deployer on the space factory");
-  if (await spaceFactory.isTrusted(deployer)) {
-    await (await spaceFactory.setIsTrusted(deployer, false)).wait();  
-  }
-
-  log("Untrust deployer on the periphery");
-  if (await periphery.isTrusted(deployer)) {
-    await (await periphery.setIsTrusted(deployer, false)).wait();  
-  }
-
-  log("Untrust deployer on the emergency stop");
-  if (await emergencyStop.isTrusted(deployer)) {
-    await (await emergencyStop.setIsTrusted(deployer, false)).wait();  
-  }
-
-  log("\n-------------------------------------------------------")
-  log("\Sanity checks: checking deployer address cannot execute trusted functions anymore...");
+  await hre.network.provider.request({
+    method: "hardhat_impersonateAccount",
+    params: [multisig],
+  });
 
   const calls = [
     tokenHandler.callStatic.setIsTrusted(dev, false),
@@ -94,10 +127,10 @@ module.exports = async function () {
     emergencyStop.callStatic.setIsTrusted(dev, false),
   ];
   const res = await Promise.allSettled(calls);
-  if (res.every(r => r.status === 'rejected')) {
-    log("\Sanity checks: OK!");
+  if (res.every(r => r.status === "fulfilled")) {
+    log("Sanity checks: OK!");
   } else {
-    throw Error('Sanity checks: FAILED');
+    throw Error("Sanity checks: FAILED");
   }
 
 };
