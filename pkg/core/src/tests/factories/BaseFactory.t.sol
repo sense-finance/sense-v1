80684b3f0e4d1760328325130e56d46c8956f79e
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2022-08-17 12:46:39 +0100

    chore: use 2e18 as sacle on guard tests

--- pkg/core/src/tests/factories/BaseFactory.t.sol
+++ pkg/core/src/tests/factories/BaseFactory.t.sol
@@ -13,6 +13,7 @@ import { BaseFactory } from "../../adapters/abstract/factories/BaseFactory.sol";
 import { FixedMath } from "../../external/FixedMath.sol";
 import { Errors } from "@sense-finance/v1-utils/src/libs/Errors.sol";
 import { Constants } from "../test-helpers/Constants.sol";
+import { Bytes32AddressLib } from "@rari-capital/solmate/src/utils/Bytes32AddressLib.sol";
 
 contract MockRevertAdapter is MockAdapter {
     constructor(BaseAdapter.AdapterParams memory _adapterParams)
@@ -30,6 +31,54 @@ contract MockRevertFactory is MockFactory {
     function deployAdapter(address _target, bytes memory data) external override returns (address adapter) {
         BaseAdapter.AdapterParams memory adapterParams;
         adapter = address(new MockRevertAdapter(adapterParams));
+        _setGuard(adapter);
+    }
+}
+
+// Mock adapter with 2e18 initial scale value
+contract Mock2e18Adapter is MockCropAdapter {
+    constructor(
+        address _divider,
+        address _target,
+        address _underlying,
+        uint128 _ifee,
+        AdapterParams memory _adapterParams,
+        address _reward
+    ) MockCropAdapter(_divider, _target, _underlying, _ifee, _adapterParams, _reward) {
+        INITIAL_VALUE = 2e18;
+    }
+}
+
+contract Mock2e18Factory is MockCropFactory {
+    using Bytes32AddressLib for address;
+
+    constructor(
+        address _divider,
+        FactoryParams memory _factoryParams,
+        address _reward
+    ) MockCropFactory(_divider, _factoryParams, _reward) {}
+
+    function deployAdapter(address _target, bytes memory data) external override returns (address adapter) {
+        BaseAdapter.AdapterParams memory adapterParams = BaseAdapter.AdapterParams({
+            oracle: factoryParams.oracle,
+            stake: factoryParams.stake,
+            stakeSize: factoryParams.stakeSize,
+            minm: factoryParams.minm,
+            maxm: factoryParams.maxm,
+            mode: factoryParams.mode,
+            tilt: factoryParams.tilt,
+            level: DEFAULT_LEVEL
+        });
+        adapter = address(
+            new Mock2e18Adapter{ salt: _target.fillLast12Bytes() }(
+                divider,
+                _target,
+                MockTargetLike(_target).underlying(),
+                factoryParams.ifee,
+                adapterParams,
+                reward
+            )
+        );
         _setGuard(adapter);
     }
 }
@@ -85,8 +134,34 @@ contract Factories is TestHelper {
 
         address[] memory rewardTokens = new address[](1);
         rewardTokens[0] = address(someReward);
-        MockCropFactory someFactory = MockCropFactory(deployCropsFactory(address(someTarget), rewardTokens, false));
 
+        // As we want to test guard calculation with a scale value different than 1e18 (which is the one used
+        // on the mock contracts), we use the Mock2e18Factory which uses deploys Mock2e18Adapter adapters.
+        // If we are testing with 4626, we can use the deployCropsFactory helper (we are not using
+        // mocks on this case). NOTE: trying to use hevm.mockCall to mock the scale call is not supported
+        // as we would be mocking the adapter with a pre-computed address and does not work.
+        MockCropFactory someFactory;
+        if (is4626) {
+            someFactory = MockCropFactory(deployCropsFactory(address(someTarget), rewardTokens, false));
+        } else {
+            BaseFactory.FactoryParams memory factoryParams = BaseFactory.FactoryParams({
+                stake: address(stake),
+                oracle: ORACLE,
+                ifee: ISSUANCE_FEE,
+                stakeSize: STAKE_SIZE,
+                minm: MIN_MATURITY,
+                maxm: MAX_MATURITY,
+                mode: MODE,
+                tilt: 0,
+                guard: DEFAULT_GUARD
+            });
+            someFactory = new Mock2e18Factory(address(divider), factoryParams, address(someReward));
+            someFactory.supportTarget(address(someTarget), true);
+            divider.setIsTrusted(address(someFactory), true);
+            periphery.setFactory(address(someFactory), true);
+        }
+
+        // deploy adapter via factory
         divider.setPeriphery(alice);
         MockCropAdapter adapter = MockCropAdapter(
             someFactory.deployAdapter(address(someTarget), abi.encode(rewardTokens))
@@ -110,8 +185,10 @@ contract Factories is TestHelper {
         } else {
             assertEq(adapter.reward(), address(someReward));
         }
+
+        // assert scale is 2e18 (or 1e18 if 4626)
         uint256 scale = adapter.scale();
-        assertEq(scale, 1e18);
+        assertEq(scale, is4626 ? 1e18 : 2e18);
 
         // Guard has been calculated with underlying-ETH (18 decimals),
         // target-underlying (18 decimals) and ETH-USD (8 decimals)
@@ -134,20 +211,24 @@ contract Factories is TestHelper {
         assertEq(guard, guardInTarget);
 
         // Sanity checks with constants values:
-        // as underlying price is 1e18, ETH price is 1900 (in 8 decimals) and scale is 1e18,
-        // (1) Underlying-USD price should be: 1e18 * (1900 * 1e8) -> normalised to 18 decimals = 1900 * 10**tDecimals
-        // (2) Target-USD price shuold be: 1e18 * (1900 * 1e18) -> normalised to 18 decimals decimals = 1900 * 10**tDecimals
+        // as underlying price is 1e18, ETH price is 1900 (in 8 decimals) and scale is 2e18,
+        // (1) Underlying-USD price should be: 1e18 * (1900 * 1e8) -> normalised to 18 decimals = 1900 * 1e18
+        // (2) Target-USD price should be: 2e18 * (1900 * 1e18) -> normalised to 18 decimals decimals = 3800 * 1e18
         // (3) and as the guard factory is $100'000 in 18 decimals
-        // then guard should be: (100000 * 1e18) / (1900 * 1e18) -> normalised to target decimals =
+        // then guard should be: (100000 * 1e18) / (3800 * 1e18) -> normalised to target decimals =
+        // * for 6 decimals target -> 26315789
+        // * for 8 decimals target -> 2631578947
+        // * for 18 decimals target -> 26315789473684210526
+        // NOTE: if 4626 tests, sacle value is 1e18 so values would be:
         // * for 6 decimals target -> 52631578
         // * for 8 decimals target -> 5263157894
         // * for 18 decimals target -> 52631578947368421052
         if (mockTargetDecimals == 6) {
-            assertEq(guard, 52631578);
+            assertEq(guard, is4626 ? 52631578 : 26315789);
         } else if (mockTargetDecimals == 8) {
-            assertEq(guard, 5263157894);
+            assertEq(guard, is4626 ? 5263157894 : 2631578947);
         } else if (mockTargetDecimals == 18) {
-            assertEq(guard, 52631578947368421052);
+            assertEq(guard, is4626 ? 52631578947368421052 : 26315789473684210526);
         }
     }
 
