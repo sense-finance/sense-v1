f91bdfefed7008b6239d126237409cac1cf73df8
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2022-09-26 10:20:32 +0100

    chore: remove yield-daddy

--- pkg/core/lib/yield-daddy/src/aave-v2/external/IAaveMining.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.4;
-
-interface IAaveMining {
-    function claimRewards(address[] calldata assets, uint256 amount, address to) external returns (uint256);
-}
--- pkg/core/lib/yield-daddy/src/lido/external/IStETH.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.13;
-
-import {ERC20} from "solmate/tokens/ERC20.sol";
-
-abstract contract IStETH is ERC20 {
-    function getTotalShares() external view virtual returns (uint256);
-}
