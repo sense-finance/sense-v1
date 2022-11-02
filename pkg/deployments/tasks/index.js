af0baf967ee57afeac4b2bda3526fbf3193a301a
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2022-11-02 16:30:42 +0000

    chore: fix file name

--- pkg/deployments/tasks/index.js
+++ pkg/deployments/tasks/index.js
@@ -15,5 +15,5 @@ require("./20220922-4626-crop-factory");
 require("./20220922-4626-factory");
 require("./20220926-euler");
 require("./20221007-morpho-adapters");
-require("./20221028-factories-and-morpho");
+require("./20221031-factories-and-morpho");
 require("./20221029-LATER-periphery");
