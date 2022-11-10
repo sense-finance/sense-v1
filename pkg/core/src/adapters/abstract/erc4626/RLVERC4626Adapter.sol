1366467b24518d9ea1ba014e4be46a8070ba6076
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2022-11-10 18:12:55 +0000

    chore: add RLV adapter and factory

---
+++ pkg/core/src/adapters/abstract/erc4626/RLVERC4626Adapter.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.13;
+
+import { ERC4626Adapter } from "./ERC4626Adapter.sol";
+
+interface Opener {
+    function onSponsorWindowOpened(address, uint256) external;
+}
+
+/// @notice RLV Ownable Adapter contract for Rolling Liquidity Vaults
+contract RLVERC4626Adapter is ERC4626Adapter {
+    uint256 internal open = 1;
+
+    constructor(
+        address _divider,
+        address _target,
+        address _rewardsRecipient,
+        uint128 _ifee,
+        AdapterParams memory _adapterParams
+    ) ERC4626Adapter(_divider, _target, _rewardsRecipient, _ifee, _adapterParams) {}
+
+    function openSponsorWindow() external requiresTrust {
+        open = 2;
+        Opener(msg.sender).onSponsorWindowOpened(adapterParams.stake, adapterParams.stakeSize);
+        open = 1;
+    }
+
+    function getMaturityBounds() external view override returns (uint256, uint256) {
+        return open == 2 ? (adapterParams.minm, adapterParams.maxm) : (0, 0);
+    }
+}
--- pkg/core/src/adapters/abstract/factories/RLVERC4626Factory.sol
+++ pkg/core/src/adapters/abstract/factories/RLVERC4626Factory.sol
@@ -1,33 +1,37 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.15;
+pragma solidity 0.8.13;
 
 // Internal references
 import { Divider } from "../../../Divider.sol";
-import { ERC4626Adapter } from "../erc4626/ERC4626Adapter.sol";
 import { BaseAdapter } from "../../abstract/BaseAdapter.sol";
+import { RLVERC4626Adapter } from "../erc4626/RLVERC4626Adapter.sol";
+import { ERC4626Factory } from "./ERC4626Factory.sol";
 import { ExtractableReward } from "../../abstract/extensions/ExtractableReward.sol";
-import { BaseFactory } from "./BaseFactory.sol";
 import { Errors } from "@sense-finance/v1-utils/libs/Errors.sol";
 
 // External references
 import { Bytes32AddressLib } from "solmate/utils/Bytes32AddressLib.sol";
 
-contract ERC4626Factory is BaseFactory {
+contract RLVERC4626Factory is ERC4626Factory {
     using Bytes32AddressLib for address;
 
-    mapping(address => bool) public supportedTargets;
+    /// @notice Rolling Liquidity Vault Factory address
+    address public immutable rlvFactory;
 
     constructor(
         address _divider,
         address _restrictedAdmin,
         address _rewardsRecipient,
-        FactoryParams memory _factoryParams
-    ) BaseFactory(_divider, _restrictedAdmin, _rewardsRecipient, _factoryParams) {}
+        FactoryParams memory _factoryParams,
+        address _rlvFactory
+    ) ERC4626Factory(_divider, _restrictedAdmin, _rewardsRecipient, _factoryParams) {
+        rlvFactory = _rlvFactory;
+    }
 
-    /// @notice Deploys an ERC4626Adapter contract
+    /// @notice Deploys an RLVERC4626Adapter contract
     /// @param _target The target address
     /// @param data ABI encoded reward tokens address array
-    function deployAdapter(address _target, bytes memory data) external virtual override returns (address adapter) {
+    function deployAdapter(address _target, bytes memory data) external override returns (address adapter) {
         /// Sanity checks
         if (Divider(divider).periphery() != msg.sender) revert Errors.OnlyPeriphery();
         if (!Divider(divider).permissionless() && !supportedTargets[_target]) revert Errors.TargetNotSupported();
@@ -47,7 +51,7 @@ contract ERC4626Factory is BaseFactory {
         // This will revert if an ERC4626 adapter with the provided target has already
         // been deployed, as the salt would be the same and we can't deploy with it twice.
         adapter = address(
-            new ERC4626Adapter{ salt: _target.fillLast12Bytes() }(
+            new RLVERC4626Adapter{ salt: _target.fillLast12Bytes() }(
                 divider,
                 _target,
                 rewardsRecipient,
@@ -58,28 +62,9 @@ contract ERC4626Factory is BaseFactory {
 
         _setGuard(adapter);
 
+        // Factory must have adapter auth so that it can give auth to the RLV
+        RLVERC4626Adapter(adapter).setIsTrusted(rlvFactory, true);
+
         ExtractableReward(adapter).setIsTrusted(restrictedAdmin, true);
     }
-
-    /// @notice (Un)support target
-    /// @param _target The target address
-    /// @param supported Whether the target should be supported or not
-    function supportTarget(address _target, bool supported) external requiresTrust {
-        supportedTargets[_target] = supported;
-        emit TargetSupported(_target, supported);
-    }
-
-    /// @notice (Un)support multiple target at once
-    /// @param _targets Array of target addresses
-    /// @param supported Whether the targets should be supported or not
-    function supportTargets(address[] memory _targets, bool supported) external requiresTrust {
-        for (uint256 i = 0; i < _targets.length; i++) {
-            supportedTargets[_targets[i]] = supported;
-            emit TargetSupported(_targets[i], supported);
-        }
-    }
-
-    /* ========== LOGS ========== */
-
-    event TargetSupported(address indexed target, bool indexed supported);
 }
