d4684f67992361ca7ab8809381de901f296dc47c
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2022-11-15 11:53:10 +0000

    chore: rebase to branch with 0.8.15 bump

--- pkg/core/src/tests/factories/OwnableERC4626Factory.t.sol
+++ pkg/core/src/tests/factories/OwnableERC4626Factory.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.13;
+pragma solidity 0.8.15;
 
 import { AutoRollerFactory } from "@auto-roller/src/AutoRollerFactory.sol";
 import { AutoRoller, RollerUtils, DividerLike, OwnedAdapterLike } from "@auto-roller/src/AutoRoller.sol";
